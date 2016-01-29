9ad352a5c2d4f2d773fae0bb6f0e5a8e00f1d14c
fis <fis@fiss-Mac.local>
2016-01-29 03:38:13 -0800

    Dummy Table View

--- SpaceNews/ViewController.m
+++ SpaceNews/ViewController.m
@@ -16,8 +16,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _list=[NSArray arrayWithObjects:@"list 1",@"list 2",@"list 3",nil];
+    
+   // self.NewsTableView.delegate = self;
+    //self.NewsTableView.dataSource = self;
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [_list count];
+}
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *tableIdentifier = @ "Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:tableIdentifier];
+    if(cell == nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:tableIdentifier];
+        //cell.textLabel.text = [_list objectAtIndex:indexPath.row];
+        
+    }    cell.textLabel.text = [_list objectAtIndex:indexPath.row];
+    //cell.detailTextLabel.text =[_list objectAtIndex:indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:@"default.png"];
+    return cell;
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
