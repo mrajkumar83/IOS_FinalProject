3295c0c6f26eb7900b0ad45027f7344de33b10d1
fis <fis@fiss-Mac.local>
2016-01-28 16:17:41 -0800

    Initial Commit

---
+++ SpaceNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpaceNews
+//
+//  Created by fis on 1/28/16.
+//  Copyright (c) 2016 Epita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SpaceNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SpaceNews
+//
+//  Created by fis on 1/28/16.
+//  Copyright (c) 2016 Epita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
